3fecfe05ff7e9c3aa691944a4600495c6c0aa24d
PhamXuanToan <phamxuantoan2001@gmail.com>
2022-07-20 11:52:49 +0700

    rebuild mvc with 3tier

---
+++ src/main/java/com/quanlytintuc/controller/admin/HomeController.java
@@ -0,0 +1,32 @@
+package com.quanlytintuc.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/admin-trang-chu"})
+public class HomeController extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("views/admin/home.jsp");
+		rd.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+
+}
